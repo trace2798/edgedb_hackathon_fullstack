29e00a2a785117f96982fc81f5c303602df4ea73
trace2798 <113078518+trace2798@users.noreply.github.com>
2024-05-12 14:45:30 +0530

    added next-auth with github

---
+++ auth.ts
@@ -0,0 +1,11 @@
+import { EdgeDBAdapter } from '@auth/edgedb-adapter';
+import { createClient } from 'edgedb';
+import NextAuth from 'next-auth';
+import Github from 'next-auth/providers/github';
+
+export const client = createClient();
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: EdgeDBAdapter(client),
+  providers: [Github]
+});
